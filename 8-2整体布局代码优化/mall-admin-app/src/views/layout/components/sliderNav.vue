af02aa0e738ad63209aa5971715dc59f67dd1101
Your Name <email@example.com>
2022-09-14 17:04:37 +0800

    8-2整体布局代码优化

---
+++ 8-2整体布局代码优化/mall-admin-app/src/views/layout/components/sliderNav.vue
@@ -0,0 +1,38 @@
+<template>
+  <!-- 头部区域 -->
+  <div class="main-header">
+    <a-button type="primary" style="margin-bottom: 16px" @click="toggleCollapsed">
+      <a-icon :type="$store.state.collapsed ? 'menu-unfold' : 'menu-fold'" />
+    </a-button>
+    <!-- 面包屑 -->
+    <div class="breadcrumb">
+      <a-breadcrumb>
+        <a-breadcrumb-item>首页</a-breadcrumb-item>
+        <a-breadcrumb-item><a href="">统计</a></a-breadcrumb-item>
+      </a-breadcrumb>
+    </div>
+    <!-- 用户信息 -->
+    <ul class="user-info">
+      <li>欢迎，阿威</li>
+      <li>退出</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      collapsed: false,
+    };
+  },
+  methods: {
+    toggleCollapsed() {
+      // this.collapsed = !this.collapsed;
+      this.$store.dispatch('changeCollapsed');
+    },
+  },
+};
+</script>
+
+<style></style>
