72ad1ad27ca95c23f778edefe6a4f461d9d27ff8
Your Name <email@example.com>
2022-09-14 17:09:24 +0800

    10-2菜单栏过滤展示

---
+++ 10-2菜单栏过滤展示/mall-admin-app/src/utils/permission.js
@@ -0,0 +1,56 @@
+/**
+ * 存储的是 角色对应的权限名称
+ */
+const roleToRoute = {
+  // 角色是顾客
+  coustomer: [
+    {
+      name: 'Product',
+    },
+    {
+      name: 'ProductList',
+    },
+    {
+      name: 'ProductAdd',
+    },
+  ],
+  // 角色是管理员
+  admin: [
+    {
+      name: 'Product',
+    },
+    {
+      name: 'ProductList',
+    },
+    {
+      name: 'ProductAdd',
+    },
+    {
+      name: 'Category',
+    },
+  ],
+};
+
+/**
+ * 获取路由信息
+ * @param {String} role  角色
+ * @param {Array} routes  路由信息 是用户传过来的
+ */
+export default function getMenuRoutes(role, routes) {
+  console.log(role, routes);
+
+  // 取到每个角色里面的属性的属性名
+  const allowRouteName = roleToRoute[role].map((item) => item.name);
+  // 传过来的路由信息 进行过滤
+  const resultRoute = routes.filter((r) => {
+    const obj = r;
+    if (allowRouteName.indexOf(obj.name) !== -1) { // 说明传过来的路由是包含的
+      const childRoute = obj.children;
+      // 说明传过来路由的子路由也是符合条件的 是包含的
+      obj.children = childRoute.filter((c) => allowRouteName.indexOf(c.name) !== -1);
+      return true;
+    }
+    return false;
+  });
+  return resultRoute;
+}
---
+++ 10-2菜单栏过滤展示/mall-admin-app/src/views/layout/components/sliderNav.vue
@@ -0,0 +1,60 @@
+<template>
+  <!-- 头部区域 -->
+  <div class="main-header">
+    <a-button
+      type="primary"
+      style="margin-bottom: 16px"
+      @click="toggleCollapsed"
+    >
+      <a-icon :type="$store.state.collapsed ? 'menu-unfold' : 'menu-fold'" />
+    </a-button>
+    <!-- 面包屑 -->
+    <div class="breadcrumb">
+      <a-breadcrumb v-if="currentRoute.length > 1">
+        <a-breadcrumb-item>{{currentRoute[0]?currentRoute[0].meta.title:''}}</a-breadcrumb-item>
+        <a-breadcrumb-item>
+          <router-link :to="{name:currentRoute[1].name}">
+            {{currentRoute[1].meta.title}}
+          </router-link>
+        </a-breadcrumb-item>
+      </a-breadcrumb>
+    </div>
+    <!-- 用户信息 -->
+    <ul class="user-info">
+      <li class="user-name">
+        {{ $store.state.user.username }}
+        <a-icon type="down"></a-icon>
+      </li>
+      <li class="login-out" @click="loginOuthandle">退出登录</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentRoute: this.$router.currentRoute.matched,
+    };
+  },
+  watch: {
+    $route() {
+      console.log(this.$router);
+      this.currentRoute = this.$router.currentRoute.matched;
+    },
+  },
+  methods: {
+    toggleCollapsed() {
+      // this.collapsed = !this.collapsed;
+      this.$store.dispatch('changeCollapsed');
+    },
+    loginOuthandle() {
+      this.$store.dispatch('loginout');
+      this.$message.warn('已退出登录');
+      this.$router.push('/login');
+    },
+  },
+};
+</script>
+
+<style></style>
